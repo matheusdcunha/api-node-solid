0f0491e823d6303bfca955beccf03ed65b8d98b2
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-04-25 00:49:19 -0300

    feat: NPM, ESLint e Validação de Env configurados.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { app } from './app'
+import { env } from './env'
 
-const port = 3333
+const port = env.PORT
 
-app.listen({
-  host: '0.0.0.0',
-  port 
-}).then(()=>{
-  console.log(`🚀 HTTP Server Running on port: ${port}!`)
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port,
+  })
+  .then(() => {
+    console.log(`🚀 HTTP Server Running on port: ${port}!`)
+  })
