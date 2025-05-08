e53eba557759252fc50134770384ef84f4b933c8
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-05-07 22:36:24 -0300

    feat: Configuração Docker Compose e Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    email: 'email@email.com',
+    name: 'Matheus',
+  },
+})
 
 export const app = fastify()
