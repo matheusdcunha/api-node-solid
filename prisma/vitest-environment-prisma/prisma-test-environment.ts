12870f0fd2a2684a4d3afe5ed8a5c9a8a3024461
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-06-21 21:20:09 -0300

    feat: Testes e2e de profile, register e authenticate

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,44 @@
+import { prisma } from '@/lib/prisma'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+import type { Environment } from 'vitest/environments'
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL env variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    // Criar o banco de teste
+
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
+
+    return {
+      async teardown() {
+        // Apagar o banco de testes
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
